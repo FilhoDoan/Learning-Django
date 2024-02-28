cf51e3d9f3f8cedd6ce643fb2892260f59d1dd93
FilhoDoan <doangalvaofilho@gmail.com>
2024-02-28 19:04:24 -0300

    adicionando put com erro de JSON parse error

--- api/serializers.py
+++ api/serializers.py
@@ -7,4 +7,4 @@ from loginUser.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['id','name','age']
--- api/views.py
+++ api/views.py
@@ -2,15 +2,27 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .serializers import UserSerializer
 from loginUser.models import User
+from rest_framework import status
 
-@api_view(['GET','POST'])
-def UserMethods(request):
+@api_view(['GET','POST','PUT'])
+def UserMethods(request,):
     if request.method == 'GET':        
         user = User.objects.all()
         serializer = UserSerializer(user, many=True)
         return Response(serializer.data)
+    
     elif request.method == 'POST':
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-        return Response(serializer.data)
+            return Response(serializer.data, status = status.HTTP_201_CREATED) 
+        return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST) 
+
+    elif request.method == 'PUT':
+        serializer = UserSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST) 
+            
+    
